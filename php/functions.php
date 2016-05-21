400d33ffbdacc3ee2e036cd4677fd588416c1dcb
Jacob Ford <j@cobford.com>
2016-05-21 11:22:36 -0400

    unsure if it matters, but title first, then viewport, then css

--- php/functions.php
+++ php/functions.php
@@ -3,11 +3,11 @@
 function htmlhead($title) {
 	$htmltitle = htmlentities($title);
 	$html = <<<HTML
+	<title>{$htmltitle}</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link rel="stylesheet" type="text/css" href="../../css/typography.css">
 	<link rel="stylesheet" type="text/css" href="../../css/stylish.css">
 	<link rel="stylesheet" type="text/css" href="../../css/stripes.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title>{$htmltitle}</title>
 HTML;
 	echo $html;
 }
