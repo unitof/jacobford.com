4dc7362e213963a79d773197c5f7b31469a926db
Jacob Ford <j@cobford.com>
2016-05-21 10:56:39 -0400

    htmlhead() now auto-encodes HTML entities

--- php/functions.php
+++ php/functions.php
@@ -1,12 +1,13 @@
 <?php
 
 function htmlhead($title) {
+	$htmltitle = htmlentities($title);
 	$html = <<<HTML
 	<link rel="stylesheet" type="text/css" href="../../css/typography.css">
 	<link rel="stylesheet" type="text/css" href="../../css/stylish.css">
 	<link rel="stylesheet" type="text/css" href="../../css/stripes.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title>{$title}</title>
+	<title>{$htmltitle}</title>
 HTML;
 	echo $html;
 }
