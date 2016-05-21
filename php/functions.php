0acd3fa7756117ece42bb6c8fc11bc61e22383cd
Jacob Ford <j@cobford.com>
2016-05-21 10:14:40 -0400

    variables need dollar signs and returns need to be echoed

--- php/functions.php
+++ php/functions.php
@@ -1,14 +1,14 @@
 <?php
 
-function jf_htmlhead(title) {
-	html = <<<HTML
+function jf_htmlhead($title) {
+	$html = <<<HTML
 	<link rel="stylesheet" type="text/css" href="../../css/typography.css">
 	<link rel="stylesheet" type="text/css" href="../../css/stylish.css">
 	<link rel="stylesheet" type="text/css" href="../../css/stripes.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>{$title}</title>
 HTML;
-	return html;
+	echo $html;
 }
 
 ?>
