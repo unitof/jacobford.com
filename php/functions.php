a9eec6560be6afd6d7ee1aa05aaab71b8559ea81
Jacob Ford <j@cobford.com>
2016-05-21 12:00:18 -0400

    htmlhead() takes a  varibale and determines proper relative paths

--- php/functions.php
+++ php/functions.php
@@ -1,14 +1,37 @@
 <?php
 
-function htmlhead($title) {
+function htmlhead($title, $pageClass = "") {
 	$htmltitle = htmlentities($title);
-	$html = <<<HTML
-	<title>{$htmltitle}</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<link rel="stylesheet" type="text/css" href="../../css/typography.css">
-	<link rel="stylesheet" type="text/css" href="../../css/stylish.css">
-	<link rel="stylesheet" type="text/css" href="../../css/stripes.css">
+	switch ($pageClass) {
+		case "project":
+			$html = <<<HTML
+			<title>{$htmltitle}</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<link rel="stylesheet" type="text/css" href="../../css/typography.css">
+			<link rel="stylesheet" type="text/css" href="../../css/stylish.css">
 HTML;
+			break;
+
+		case "index":
+			$html = <<<HTML
+			<title>{$htmltitle}</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<link rel="stylesheet" type="text/css" href="css/typography.css">
+			<link rel="stylesheet" type="text/css" href="css/stylish.css">
+			<link rel="stylesheet" type="text/css" href="css/stripes.css">
+HTML;
+			break;
+
+		default:
+			$html = <<<HTML
+			<title>{$htmltitle}</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<link rel="stylesheet" type="text/css" href="/css/typography.css">
+			<link rel="stylesheet" type="text/css" href="/css/stylish.css">
+			<link rel="stylesheet" type="text/css" href="/css/stripes.css">
+HTML;
+			break;
+	}
 	echo $html;
 }
 
