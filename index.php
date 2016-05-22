43d6d2c5b46e45309b42a254b9fdc22d19922607
Jacob Ford <j@cobford.com>
2016-05-22 19:21:10 -0400

    phpify index.php for htmlindexstrip()

--- index.php
+++ index.php
@@ -13,28 +13,10 @@
 <div id="container">
 <?php htmlheader($pageClass) ?>
 	<div id="stripestack">
-		<a class="portfolio-item" href="projects/deathstrip/" id="deathstrip">
-			<div class="image-strip" style="background-image: url(../projects/deathstrip/str_deathstrip.jpg);"></div>
-			<h3 class="item-title">Death Strip: A Map of Berlin &amp; Time</h3>
-		</a>
-		<a class="portfolio-item" href="projects/violet100/" id="violet100">
-			<div class="image-strip" style="background-image: url(../projects/violet100/str_violet100.jpg);"></div>
-			<h3 class="item-title">Violet 100: The NYU Spirit Week
-				<span class="item-client"> for the NYU Student Senators Council</span>
-			</h3>
-		</a>
-		<a class="portfolio-item" href="projects/urbandemocracylab/" id="urbandemocracylab">
-			<div class="image-strip" style="background-image: url(../projects/urbandemocracylab/str_urbandemocracylab.png);"></div>
-			<h3 class="item-title">Civic Engagement Posters
-				<span class="item-client"> for the Urban Democracy Lab</span>
-			</h3>
-		</a>
-		<a class="portfolio-item" href="projects/gallatinfacultyshow/" id="gallatinfacultyshow">
-			<div class="image-strip" style="background-image: url(../projects/gallatinfacultyshow/str_gallatinfacultyshow.jpg);"></div>
-			<h3 class="item-title">Gallatin Faculty Show
-				<span class="item-client"> for the Gallatin Galleries</span>
-			</h3>
-		</a>
+<?php htmlindexstrip("deathstrip", "Death Strip: A Map of Berlin &amp; Time", "", "jpg"); ?>
+<?php htmlindexstrip("violet100", "Violet 100: The NYU Spirit Week", "for the NYU Student Senators Council", "jpg"); ?>
+<?php htmlindexstrip("urbandemocracylab", "Civic Engagement Posters", "for the Urban Democracy Lab", "png"); ?>
+<?php htmlindexstrip("gallatinfacultyshow", "Gallatin Faculty Show", "for the Gallatin Galleries", "jpg"); ?>
 	</div>
 </div>
 </body>
