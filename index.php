10b0e108677bfd893325c34dfdef2eff583be001
Jacob Ford <j@cobford.com>
2016-05-22 18:57:21 -0400

    move id to a.portfolio-item & put background-images inline

--- index.php
+++ index.php
@@ -13,24 +13,24 @@
 <div id="container">
 <?php htmlheader($pageClass) ?>
 	<div id="stripestack">
-		<a class="portfolio-item" href="projects/deathstrip/">
-			<div class="image-strip" id="deathstrip"></div>
+		<a class="portfolio-item" href="projects/deathstrip/" id="deathstrip">
+			<div class="image-strip" style="background-image: url(../projects/deathstrip/str_deathstrip.jpg);"></div>
 			<h3 class="item-title">Death Strip: A Map of Berlin &amp; Time</h3>
 		</a>
-		<a class="portfolio-item" href="projects/violet100/">
-			<div class="image-strip" id="violet100"></div>
+		<a class="portfolio-item" href="projects/violet100/" id="violet100">
+			<div class="image-strip" style="background-image: url(../projects/violet100/str_violet100.jpg);"></div>
 			<h3 class="item-title">Violet 100: The NYU Spirit Week
 				<span class="item-client"> for the NYU Student Senators Council</span>
 			</h3>
 		</a>
-		<a class="portfolio-item" href="projects/urbandemocracylab/">
-			<div class="image-strip" id="urbandemocracylab"></div>
+		<a class="portfolio-item" href="projects/urbandemocracylab/" id="urbandemocracylab">
+			<div class="image-strip" style="background-image: url(../projects/urbandemocracylab/str_urbandemocracylab.png);"></div>
 			<h3 class="item-title">Civic Engagement Posters
 				<span class="item-client"> for the Urban Democracy Lab</span>
 			</h3>
 		</a>
-		<a class="portfolio-item" href="projects/gallatinfacultyshow/">
-			<div class="image-strip" id="gallatinfacultyshow"></div>
+		<a class="portfolio-item" href="projects/gallatinfacultyshow/" id="gallatinfacultyshow">
+			<div class="image-strip" style="background-image: url(../projects/gallatinfacultyshow/str_gallatinfacultyshow.jpg);"></div>
 			<h3 class="item-title">Gallatin Faculty Show
 				<span class="item-client"> for the Gallatin Galleries</span>
 			</h3>
