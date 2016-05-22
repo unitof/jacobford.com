9ff0e17697d614b1e2af3a00422657c33b15bcd2
Jacob Ford <j@cobford.com>
2016-05-22 12:39:54 -0400

    new htmlfooter() function to insert div#footer

--- php/functions.php
+++ php/functions.php
@@ -52,4 +52,26 @@ HTML;
 	echo $html . "\n";
 }
 
+function htmlfooter($returnTo = "the Index") {
+	$html = <<<HTML
+	<div id="footer" class="project-footer">
+		<!-- TODO: This a element prevents the
+		     margin collapsing that should be
+		     happening with div#footer. Any fix? -->
+		<a href="../../" class="nav-returnlink no-dec">
+			<div class="icon-index">
+				<div class="icon-index-stripe"></div>
+				<div class="icon-index-stripe icon-index-expand"></div>
+				<div class="icon-index-stripe"></div>
+				<div class="icon-index-stripe"></div>
+				<div class="icon-index-stripe"></div>
+			</div>
+			<h2>Return <span class="lc">to</span> {$returnTo}</h2>
+		</a>
+	</div>
+HTML;
+
+	echo $html . "\n";
+}
+
 ?>
