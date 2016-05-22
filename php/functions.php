3cac8625097788bc12a7656d06242063fbbcde48
Jacob Ford <j@cobford.com>
2016-05-22 18:58:39 -0400

    remove stripes.css and all references to it

--- php/functions.php
+++ php/functions.php
@@ -42,7 +42,6 @@ HTML;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/typography.css">
 	<link rel="stylesheet" type="text/css" href="css/stylish.css">
-	<link rel="stylesheet" type="text/css" href="css/stripes.css">
 HTML;
 			break;
 
@@ -52,7 +51,6 @@ HTML;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="/css/typography.css">
 	<link rel="stylesheet" type="text/css" href="/css/stylish.css">
-	<link rel="stylesheet" type="text/css" href="/css/stripes.css">
 HTML;
 			break;
 	}
