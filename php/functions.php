ea883ad702cbde4ea395725331e5bf3e5447ca45
Jacob Ford <j@cobford.com>
2016-05-22 22:09:15 -0400

    include traffic with developer cookie, but mark with custom dimension

--- php/functions.php
+++ php/functions.php
@@ -13,16 +13,15 @@ function htmlhead($pageClass = "", $title = "") {
 	$htmltitle = htmlspecialchars($title);
 	$ga = <<<HTML
 	<script>
-	if (document.cookie.includes("JFDev=dev") == false) {
-		if (document.location.hostname.search(/^(.+\.)?jacobford\.com/i) !== -1) {
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-78108122-1', 'auto');
-			ga('send', 'pageview');
-		} else { console.log("Google Analytics disabled: hostname not jacobford.com"); }
-	} else { console.log("Google Analytics disabled: developer cookie detected"); }
+	if (document.location.hostname.search(/^(.+\.)?jacobford\.com/i) !== -1) {
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		ga('create', $propId, 'auto');
+		ga('send', 'pageview');
+	} else { console.log("Google Analytics disabled: hostname not jacobford.com"); }
+	if (document.cookie.includes("JFDev=dev") == true ) { ga('set', 'dimension1', 'true'); }
 	</script>
 HTML;
 
