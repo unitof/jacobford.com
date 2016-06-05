7fc2c1d77672309046cc3a0814819b6533680eb6
Jacob Ford <j@cobford.com>
2016-06-05 15:49:52 -0400

    restructure card HTML following desandro's tutorial

--- card/index.php
+++ card/index.php
@@ -12,16 +12,18 @@
 <body ontouchstart>
 <div id="container">
 <?php htmlheader($pageClass) ?>
-	<div class="card" id="front">
-		<p class="contact email"><a class="hidden" href="mailto:j@cobford.com">j@cobford.com</a></p>
-		<p class="contact phone">(<span class="three digits expand">best to <a href="mailto:j@cobford.com">email me</a> first</span>)<span class="subscribernumber digits contract"><span class="three digits">000</span>-<span class="four digits">0000</span></span></p>
-	</div>
-	<!--
-	<div class="card" id="back">
-		<h2 class="name">Jacob Ford</h2>
-		<h3 class="title">Designer&nbsp;<span class="lc">of</span> Things</h3>
-	</div>
-	-->
+	<section class="card-holder">
+		<div class="card">
+			<div class="card-face" id="back">
+				<p class="contact email"><a class="hidden" href="mailto:j@cobford.com">j@cobford.com</a></p>
+				<p class="contact phone">(<span class="three digits expand">best to <a href="mailto:j@cobford.com">email me</a> first</span>)<span class="subscribernumber digits contract"><span class="three digits">000</span>-<span class="four digits">0000</span></span></p>
+			</div>
+			<div class="card-face" id="front">
+				<h2 class="name">Jacob Ford</h2>
+				<h3 class="title">Designer&nbsp;<span class="lc">of</span> Things</h3>
+			</div>
+		</div>
+	</section>
 </div>
 </body>
 </html>
