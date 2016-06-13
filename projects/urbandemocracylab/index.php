4edf2f743a53e962ba0bbafe9c31ea1d279db679
Jacob Ford <j@cobford.com>
2016-06-13 14:22:25 -0400

    srcset and improved alt tags for UDL

--- projects/urbandemocracylab/index.php
+++ projects/urbandemocracylab/index.php
@@ -18,15 +18,15 @@
 <?php htmlheader($pageClass, $middle, $bottom, true); ?>
 	<div id="story">
 		<div class="overhang">
-			<img src="mfte.png" alt="Marching for the Extraordinary: a panel dicussion on the culture and spectacle of contemporary street bands.">
+			<img src="mfte.png" srcset="mfte.svg" alt="Marching for the Extraordinary: a panel dicussion on the culture and spectacle of contemporary street bands.">
 		</div>
 		<p>A research laboratory founded by NYU Gallatin, the Urban Democracy Lab &ldquo;promotes critical, creative, just, and sustainable forms of urbanism, through scholarship, curricular activities, public engagement, and programming.&rdquo; We plan events that make people think, and I tried to make our posters do the same. Most were displayed in the Broadway-facing windows at Gallatin&rsquo;s One Washington Place lobby. Some included companion MailChimp templates for email campaigns.</p>
 		<div class="overhang">
-			<img src="war.svg" alt="We Are Revolutionaries" onerror="this.onerror=null; this.src='war.png'">
+			<img src="war.svg" srcset="war.svg" alt="We Are Revolutionaries: What / black power / tells us / about / Democracy / in America; left size slants to the right, right side slants to the right">
 			<div class="gallery">
-				<div class="frame"><img src="war-reject1.png" alt=""></div>
-				<div class="frame"><img src="war-reject2.png" alt=""></div>
-				<div class="frame"><img src="war-reject3.png" alt=""></div>
+				<div class="frame"><img src="war-reject1.png" srcset="war-reject1.svg" alt="Dark grey background, WE ARE REVOLUTIONARIES with the last word inscribed in a black circle. Sponsor logos, event details, and a paragraph of text radiate from the circle perpendicular to its tangent."></div>
+				<div class="frame"><img src="war-reject2.png" srcset="war-reject2.svg" alt="Dark grey background, WE ARE REVOLUTIONARIES with the last word inscribed in a red circle. Sponsor logos, event details, and a paragraph of text radiate from the circle perpendicular to its tangent."></div>
+				<div class="frame"><img src="war-reject3.png" srcset="war-reject3.svg" alt="Orange background, WE ARE REVOLUTIONARIES with the last word inscribed in a black circle. Sponsor logos radiate from the circle perpendicular to its tangent, and event details run from the word ARE."></div>
 			</div>
 			<p class="caption">Top: final design for We Are Revolutionaries<br>Bottom: three rejected designs made along the way</p>
 		</div>
@@ -37,35 +37,35 @@
 			<li>The reader: <i>now</i> we may expect the viewer to read blocks of text, but only very special viewers make it this far. They deserve something succint and well-composed. Details ready to be added to a calendar.</li>
 		</ol></p>
 		<div>
-			<img class="border" src="iol.png" alt="Infrastructures of Labor: grey boxes stacked like a newspaper">
+			<img class="border" src="iol.png" srcset="iol.svg" alt="Infrastructures of Labor: grey boxes stacked like a newspaper">
 			<p class="caption">Infrastructures of Labor</p>
 		</div>
 		<div>
-			<img src="lis.png" alt="Life is Sacred: bright yellow and grey and about Antanas Mockus">
+			<img src="lis.png" srcset="lis.svg" alt="Life is Sacred: bright yellow and grey and about Antanas Mockus">
 			<p class="caption">Life is Sacred</p>
 		</div>
 		<div>
-			<img src="trthr.png" alt="The Right to Have Rights: Citizenship and the Future of Cities, in red, maroon, and very light pink where the Is are stacked and vertically joined">
+			<img src="trthr.png" srcset="trthr.svg" alt="The Right to Have Rights: Citizenship and the Future of Cities, in red, maroon, and very light pink where the Is are stacked and vertically joined">
 			<p class="caption">The Right to Have Rights</p>
 		</div>
 		<div>
-			<img src="to.png" alt="The Occupiers: The Making of a 99 Percent Movement in four shades of grey">
+			<img src="to.png" srcset="to.svg" alt="The Occupiers: The Making of a 99 Percent Movement in four shades of grey">
 			<p class="caption">The Occupiers</p>
 		</div>
 		<div>
-			<img src="tftt.png" alt="The Fire This Time: Decolonizing Higher Education, in red and very light pink">
+			<img src="tftt.png" srcset="tftt.svg" alt="The Fire This Time: Decolonizing Higher Education, in red and very light pink">
 			<p class="caption">The Fire This Time</p>
 		</div>
 		<div>
-			<img src="ior.png" alt="Infrustructures of Resiliance: dark grey and bright red and an ampersand">
+			<img src="ior.png" srcset="ior.svg" alt="Infrustructures of Resiliance: dark grey and bright red and an ampersand">
 			<p class="caption">Infrustructures of Resiliance</p>
 		</div>
 		<div>
-			<img src="nrwu.png" alt="No Revolution Without Us: Feminists of the Black Panther Party, hot pink and dark black split down the middle; Black Panthers right, Feminists left">
+			<img src="nrwu.png" srcset="nrwu.svg" alt="No Revolution Without Us: Feminists of the Black Panther Party, hot pink and dark black split down the middle; Black Panthers right, Feminists left">
 			<p class="caption">No Revolution Without Us</p>
 		</div>
 		<div>
-			<img src="stpm.png" alt="(Still) the Progressive Mayor?: Bill de Blasio in Year Two.">
+			<img src="stpm.png" srcset="stpm.svg" alt="(Still) the Progressive Mayor?: Bill de Blasio in Year Two. Varying weights of hot pink Helvetica on yellow hierarchize information.">
 			<p class="caption">(Still) the Progressive Mayor?</p>
 		</div>
 		<div class="credits">
