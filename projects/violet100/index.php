dae82d08b6741cb268984c2d497ec34046cf5e79
Jacob Ford <j@cobford.com>
2016-06-13 22:04:25 -0400

    purged violet100_ prefix on V100 images

--- projects/violet100/index.php
+++ projects/violet100/index.php
@@ -18,28 +18,28 @@
 <?php htmlheader($pageClass, $middle, $bottom); ?>
 	<div id="story">
 		<div class="overhang">
-			<img src="violet100_kimmel_straight.jpg" alt="A map depicting the Death Strip slicing through Berlin.">
+			<img src="kimmel_straight.jpg" alt="A map depicting the Death Strip slicing through Berlin.">
 		</div>
 		<p>Originally it stood for 100 hours of NYU spirit. Now it is a weeklong series of events celebrating university pride, and students needed to know it was happening.</p>
-		<img src="violet100_thegrid.png" alt="VIO / LET / 100">
+		<img src="thegrid.png" alt="VIO / LET / 100">
 		<p>I developed what become known as the Tic-Tac-Toe identity (though I strongly preferred The Grid), to be used across all promotional material. It showed up on coffee cup sleeves, napkin dispensers in the university cafeterias, hats, shrits, and on all flyers for the individual events which Violet 100 comprised.</p>
 		<p>Then we decided to make a sticker the size of a building.</p>
 		<div class="superoverhang">
-			<img src="violet100_kimmel_facademockup.svg" onerror="this.onerror=null; this.src='violet100_kimmel_facademockup.png'" alt="Installation mockup">
+			<img src="kimmel_facademockup.svg" onerror="this.onerror=null; this.src='kimmel_facademockup.png'" alt="Installation mockup">
 			<p class="caption">installation mockup</p>
 		</div>
 		<!-- <p>On a cold windy day I went and measured all the windows and designed a template on which we could mockup our designs. Two schedules pointed in from either side to a Tic Tac Toe box, and the whole thing was made amazing with a split and rotated box across all four center doors.</p> -->
 		<div class="overhang">
-			<img src="violet100_kimmel_install.jpg" alt="The install team applies the vinyl.">
+			<img src="kimmel_install.jpg" alt="The install team applies the vinyl.">
 			<p class="caption">The install team</p>
 		</div>
 		<!-- <p>John excellently vectorized two NYU Bobcats to flank the center doors, each with strips to match its neighboring schedule bars.</p> -->
 		<div class="overhang">
-			<img src="violet100_kimmel_angle.jpg" alt="VIOLET 100 stretchs across four glass panes on the front of NYU's Kimmel Center for University Life.">
+			<img src="kimmel_angle.jpg" alt="VIOLET 100 stretchs across four glass panes on the front of NYU's Kimmel Center for University Life.">
 			<p class="caption">the decal&rsquo;s first night in the wild</p>
 		</div>
 		<p>But we kept some things small, for scale.</p>
-		<img src="violet100_cellpocket.png" alt="The V100 Tic-Tac-Toe board decorates a cell phone." style="max-width: 200px;">
+		<img src="cellpocket.png" alt="The V100 Tic-Tac-Toe board decorates a cell phone." style="max-width: 200px;">
 		<div class="credits">
 			<p>Project managed by <span class="name">Petey Shivery</span>.</p>
 			<p>Flanking bobcats designed by <span class="name">John Belknap</span>.</p>
