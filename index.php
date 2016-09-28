2216149d45ee5284d7990be038e6e90c8a40aad8
Jacob Ford <j@cobford.com>
2016-09-27 23:41:37 -0400

    Reorder index for coolest color

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 <?php htmlindexstrip("deathstrip", "Death Strip: A Map of Berlin &amp; Time", "", "jpg"); ?>
 <?php htmlindexstrip("tomographictopography", "Tomographic Topography", "", "jpg"); ?>
 <?php htmlindexstrip("urbandemocracylab", "Civic Engagement Posters", "for the Urban Democracy Lab", "png"); ?>
-<?php htmlindexstrip("violet100", "Violet 100: The NYU Spirit Week", "for the NYU Student Senators Council", "jpg"); ?>
-<?php htmlindexstrip("gallatinfacultyshow", "Gallatin Faculty Show", "for the Gallatin Galleries", "jpg"); ?>
 <?php htmlindexstrip("doesmennonitematter", "Does Mennonite Matter?", "for the Franconia Conference", "jpg"); ?>
+<?php htmlindexstrip("gallatinfacultyshow", "Gallatin Faculty Show", "for the Gallatin Galleries", "jpg"); ?>
+<?php htmlindexstrip("violet100", "Violet 100: The NYU Spirit Week", "for the NYU Student Senators Council", "jpg"); ?>
 	</div>
 <?php htmlfooter($pageClass); ?>
 </div>
