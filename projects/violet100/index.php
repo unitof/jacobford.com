b895105923110b38f22416f663e11417e9f0dd46
Jacob Ford <j@cobford.com>
2016-09-27 23:20:40 -0400

    Resizing and srcsetting of V100 assets

--- projects/violet100/index.php
+++ projects/violet100/index.php
@@ -18,7 +18,8 @@
 <?php htmlheader($pageClass, $middle, $bottom); ?>
 	<div id="story">
 		<div class="overhang">
-			<img src="kimmel_straight.jpg" alt="A map depicting the Death Strip slicing through Berlin.">
+			<img <?php htmlsrcset('kimmel_straight.jpg', $pageClass, 3, true); ?> alt="A map depicting the Death Strip slicing through Berlin.">
+			<!-- Future: .jpf files are ready to go, pending wider adoption of JPEG 2000 and my own personal switch to <figure>s -->
 		</div>
 		<p>Originally it stood for 100 hours of NYU spirit. Now it is a weeklong series of events celebrating university pride, and students needed to know it was happening.</p>
 		<img src="thegrid.png" srcset="thegrid.svg" alt="VIO / LET / 100">
@@ -26,20 +27,20 @@
 		<p>Then we decided to make a sticker the size of a building.</p>
 		<div class="superoverhang">
 			<img src="kimmel_facademockup.png" srcset="kimmel_facademockup.svg" alt="Installation mockup: a drawn view of the Kimmel facade with measurements, and locations marked for the vinyl.">
-			<p class="caption">installation mockup</p>
+			<p class="caption">Installation mockup</p>
 		</div>
 		<!-- <p>On a cold windy day I went and measured all the windows and designed a template on which we could mockup our designs. Two schedules pointed in from either side to a Tic Tac Toe box, and the whole thing was made amazing with a split and rotated box across all four center doors.</p> -->
 		<div class="overhang">
-			<img src="kimmel_install.jpg" alt="The install team applies the vinyl.">
+			<img <?php htmlsrcset('kimmel_install.jpg', $pageClass, 3, true); ?> alt="The install team applies the vinyl.">
 			<p class="caption">The install team</p>
 		</div>
 		<!-- <p>John excellently vectorized two NYU Bobcats to flank the center doors, each with strips to match its neighboring schedule bars.</p> -->
 		<div class="overhang">
-			<img src="kimmel_angle.jpg" alt="VIOLET 100 stretchs across four glass panes on the front of NYU's Kimmel Center for University Life.">
-			<p class="caption">the decal&rsquo;s first night in the wild</p>
+			<img <?php htmlsrcset('kimmel_angle.jpg', $pageClass, 3, true); ?> alt="VIOLET 100 stretchs across four glass panes on the front of NYU's Kimmel Center for University Life.">
+			<p class="caption">The decal’s first night in the wild</p>
 		</div>
 		<p>But we kept some things small, for scale.</p>
-		<img src="cellpocket.png" alt="The V100 Tic-Tac-Toe board decorates a cell phone." style="max-width: 200px;">
+		<img <?php htmlsrcset('cellpocket.png', $pageClass, 3, false); ?> alt="The V100 Tic-Tac-Toe board decorates a cell phone." style="max-width: 200px;">
 		<div class="credits">
 			<p>Project managed by <span class="name">Petey Shivery</span>.</p>
 			<p>Flanking bobcats designed by <span class="name">John Belknap</span>.</p>
