d283715456d3eb5d921064285e8815758a15cd9b
Jacob Ford <j@cobford.com>
2019-09-28 00:45:58 -0400

    Add business card page check

--- test/test.js
+++ test/test.js
@@ -13,4 +13,14 @@ describe('Screenshot compare with previous deployment', function() {
     await page.goto(server.url)
     await percySnapshot(page, this.test.fullTitle()) // doesn't reject/resolve, just runs (sadly)
   })
+
+  it('Loads the business card page', async function() {
+    const server = await phpServer({
+      port: 0, //random
+    })
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(`${server.url}/card`)
+    await percySnapshot(page, this.test.fullTitle()) // doesn't reject/resolve, just runs (sadly)
+  })
 })
