ce36babdbc418ecfd3bbacb55708eddbe79272d1
Jacob Ford <j@cobford.com>
2022-11-22 13:32:20 -0500

    Remove Google Analytics

--- php/functions.php
+++ php/functions.php
@@ -32,21 +32,7 @@ function toRoot($pageClass) {
 
 function htmlhead($pageClass = "", $title = "") {
 	$htmltitle = htmlspecialchars($title);
-	$ga = <<<HTML
-	<script>
-	var hn = document.location.hostname;
-	var propId = 'UA-78108122-1';
-	if (hn == 'staging.jacobford.com') { propId = 'UA-78108122-2'; }
-	if (hn.search(/^(.+\.)?jacobford\.com/i) !== -1) {
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-		ga('create', propId, 'auto');
-		if (document.cookie.includes("JFDev=dev") == true ) { ga('set', 'dimension1', 'true'); }
-		ga('send', 'pageview');
-	} else { console.log("Google Analytics disabled: hostname not jacobford.com"); }
-	</script>
+	$analytics = <<<HTML
 	<!-- ↓ Fathom Analytics, full transparency. It’s good: usefathom.com/ref/VIGUPS -->
 	<script src="https://magpie.unitof.com/script.js" data-site="BUTONZDI" defer></script>
 HTML;
@@ -59,7 +45,7 @@ HTML;
 	<link rel="stylesheet" type="text/css" href="{$toRoot}css/typography.css">
 	<link rel="stylesheet" type="text/css" href="{$toRoot}css/stylish.css">
 HTML;
-	$html .= "\n" . $ga;
+	$html .= "\n" . $analytics;
 	echo $html . "\n";
 }
 
